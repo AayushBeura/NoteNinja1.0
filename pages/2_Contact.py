5a32e07279943728c7723fcdec47e8825968d9b9
Aayush Beura <90240872+AayushBeura@users.noreply.github.com>
2025-02-02 23:20:58 +0530

    Update 2_Contact.py

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -53,7 +53,7 @@ def contact():
         st.markdown("""
         ### Connect With Us
         [![Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/AayushBeura)
-        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/AayushBeura)
+        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/AayushBeura)
         [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/AayushBeura)
         """)
         
